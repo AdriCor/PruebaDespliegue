ff930a4a0a0948aa4d0d7c12ac34b0067b2edb15
Adri <acorrionerocastillo@gmail.com>
2023-11-16 19:15:17 +0100

    actualizo contraseña

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 $servidor = 'localhost';
 $usuario = 'root';
-$contrasena = 'acc1234';
+$contrasena = 'Acc12345';
 $base_datos = 'login';
 $conexion = new Mysqli($servidor, $usuario, $contrasena, $base_datos)
 
